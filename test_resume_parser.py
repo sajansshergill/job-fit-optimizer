977d43fa667f9e472346e59552d949a34135d839
Sajan Singh Shergill <ss865268@gmail.com>
2025-06-22 21:46:58 -0400

    deleted files

--- test_resume_parser.py
+++
@@ -1,4 +0,0 @@
-from resume_parser import parse_resume
-
-result = parse_resume("/Users/sajanshergill/Desktop/job-fit-optimizer/data/sample_resumes/SajanSinghShergill-RESUME(On-Campus).pdf")
-print(result)
